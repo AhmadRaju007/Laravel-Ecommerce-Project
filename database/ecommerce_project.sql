3fd5bc7d1fd8d7dbce1ab1d199688bf6376e2398
AhmadRaju <rajuahmad6789@gmail.com>
2021-03-12 02:31:11 +0600

    adds database

---
+++ database/ecommerce_project.sql
@@ -0,0 +1,152 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.14 
+MySQL - 5.5.5-10.4.14-MariaDB : Database - ecommerce_project
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ecommerce_project` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `ecommerce_project`;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `categories` */
+
+insert  into `categories`(`id`,`category_name`,`created_at`,`updated_at`) values (1,'Shoes','2021-02-28 12:12:12','2021-02-28 12:12:12'),(2,'T-Shirts','2021-02-28 12:12:12','2021-02-28 12:12:12'),(3,'Watches','2021-02-28 12:12:12','2021-02-28 12:12:12');
+
+/*Table structure for table `failed_jobs` */
+
+DROP TABLE IF EXISTS `failed_jobs`;
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `failed_jobs` */
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2021_02_03_194145_create_products_table',1),(5,'2021_02_14_214050_add_columns_to_product_table',2),(6,'2021_02_14_214228_create_categories_table',2);
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `address` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `transaction_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `currency` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `orders` */
+
+insert  into `orders`(`id`,`name`,`email`,`phone`,`amount`,`address`,`status`,`transaction_id`,`currency`) values (1,'Customer Name','customer@mail.com','8801XXXXXXXXX',10,'Customer Address','Processing','6047d80401f78','BDT');
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `details` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `size` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `products` */
+
+insert  into `products`(`id`,`name`,`image`,`amount`,`price`,`is_active`,`created_at`,`updated_at`,`details`,`category_id`,`size`) values (1,'Product1','/images/uploads/product_16129959641.jpg|/images/uploads/product_16129959642.jpg',2,222,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 1',1,''),(2,'Product2','/images/uploads/product-2.jpg',50,200,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 2',2,''),(3,'product3','/images/uploads/product-3.jpg',400,400,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 3',1,''),(4,'product4','/images/uploads/product-4.jpg',400,400,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 4',3,''),(5,'product5','/images/uploads/product-5.jpg',400,400,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 5',1,''),(6,'product6','/images/uploads/product-6.jpg',400,400,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 6',2,''),(7,'product7','/images/uploads/product_16129940991.jpg|/images/uploads/product_16129940992.jpg|/images/uploads/product_16129940993.jpg|/images/uploads/product_16129940994.jpg',111,100,1,'2021-02-10 20:31:06','2021-02-10 20:31:06','This is sample product no. 7',1,''),(8,'product8','/images/uploads/product_16129959641.jpg|/images/uploads/product_16129959642.jpg',200,100,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 8',2,''),(9,'product9','/images/uploads/product_16129959641.jpg|/images/uploads/product_16129959642.jpg',400,500,1,'2021-02-10 20:34:21','2021-02-10 20:34:21','This is sample product no. 9',1,'');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mobile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
+  `is_admin` tinyint(1) DEFAULT 0,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`mobile`,`email_verified_at`,`password`,`is_active`,`is_admin`,`remember_token`,`created_at`,`updated_at`) values (1,'alice','alice@alice.com','56789',NULL,'12345',1,0,NULL,'2021-02-05 03:33:51','2021-02-05 03:33:51'),(2,'raju','a@b.c','09876544',NULL,'123456',1,0,NULL,'2021-02-08 02:28:20','2021-02-08 02:28:20'),(3,'alice1','a@bc.com','01234567890',NULL,'123456',1,0,NULL,'2021-02-27 03:51:58','2021-02-27 03:51:58');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
